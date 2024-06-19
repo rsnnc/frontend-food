e105c83d7a4a016b86a8fe4e771c97963d13ce54
Renegade <sfoqus@gmail.com>
2024-06-19 10:10:44 +0300

    Added timer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-
+    //  tabs
     const tabContainer = document.querySelector('.tabcontainer'),
           tabHeaderItem = document.querySelectorAll('.tabheader__item'),
           tabContent = document.querySelector('.tabcontent');
@@ -51,4 +51,51 @@ document.addEventListener('DOMContentLoaded', () => {
     }
     changeTabContent();
     console.log(tabHeaderItem.classList); 
+    // timer
+
+    const deadline = '2024-06-25';
+
+    function getTimeRemaining(endtime) {
+        const t = Date.parse(endtime) - Date.parse(new Date()),
+              days = Math.floor(t / 1000 / 60 / 60 / 24),
+              hours = Math.floor(t / 1000 / 60 / 60 % 24),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor(t / 1000 % 60);
+        return {
+            total: t,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds
+        }
+    }
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+    function setClock(endtime) {
+        const timer = document.querySelector('.timer'),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        updateClock();
+        function updateClock() {
+            const t = getTimeRemaining(endtime);
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+    setClock(deadline);
 })
